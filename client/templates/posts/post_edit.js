51d0591c7f30002284b3e0683d40a43ae2150ecc
george dorgbetor <geomingles@yahoo.com>
2015-04-01 14:14:29 +0100

    Created basic errors package and linked it in.

--- client/templates/posts/post_edit.js
+++ client/templates/posts/post_edit.js
@@ -29,7 +29,8 @@ Template.postEdit.events({
 
 		Posts.update(currentPostId, {$set: postProperties}, function(error) {
 			if (error) {
-				throwError(error.reason);
+				//display the error to the user
+				Errors.throw(error.reason);
 			} else {
 				Router.go('postPage', {_id: currentPostId});
 			}
